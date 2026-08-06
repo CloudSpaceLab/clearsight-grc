a54d06462811b6f1c39e8bd2a0b81bf2b8a31622
xwiz <c.opata@robosyslive.com>
2026-08-06 13:39:00 +0100

    test: let React Testing Library manage act state

--- web/src/test/setup.ts
+++ web/src/test/setup.ts
@@ -2,8 +2,3 @@ import { cleanup } from "@testing-library/react";
 import { afterEach } from "vitest";
 
 afterEach(() => cleanup());
-
-Object.defineProperty(globalThis, "IS_REACT_ACT_ENVIRONMENT", {
-  configurable: true,
-  value: true,
-});
