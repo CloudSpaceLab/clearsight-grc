563dfe973722e20616df45716acc97134d257141
xwiz <c.opata@robosyslive.com>
2026-08-10 23:49:02 +0100

    use canonical route registry for identity access

--- internal/httpapi/server.go
+++ internal/httpapi/server.go
@@ -55,7 +55,6 @@ func New(deps Dependencies) http.Handler {
 	api := &API{deps: deps}
 	mux := http.NewServeMux()
 	api.registerFederationRoutes(mux)
-	api.registerIdentityAccessRoutes(mux)
 	api.registerRoutes(mux)
 	appHandler := httpx.Chain(
 		mux,
