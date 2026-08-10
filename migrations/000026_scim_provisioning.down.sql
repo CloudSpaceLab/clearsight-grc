11dadd11fb70416ba15f048b0391162b23f0742b
xwiz <c.opata@robosyslive.com>
2026-08-10 21:03:30 +0100

    add SCIM provisioning rollback

---
+++ migrations/000026_scim_provisioning.down.sql
@@ -0,0 +1,9 @@
+BEGIN;
+
+DROP TABLE IF EXISTS directory_group_role_bindings;
+DROP TABLE IF EXISTS directory_group_members;
+DROP TABLE IF EXISTS directory_groups;
+DROP TABLE IF EXISTS scim_users;
+DROP TABLE IF EXISTS scim_sources;
+
+COMMIT;
