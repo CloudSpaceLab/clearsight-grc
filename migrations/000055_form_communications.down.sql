79977708e1edf269d2d2bae09a5d8679e54c9f79
xwiz <c.opata@robosyslive.com>
2026-08-28 07:32:44 +0100

    feat: add governed communication rollback

---
+++ migrations/000055_form_communications.down.sql
@@ -0,0 +1,8 @@
+BEGIN;
+
+DROP TABLE IF EXISTS form_delivery_attempts;
+DROP TABLE IF EXISTS form_communication_templates;
+DROP TABLE IF EXISTS form_communication_profiles;
+DROP TABLE IF EXISTS form_brand_assets;
+
+COMMIT;
