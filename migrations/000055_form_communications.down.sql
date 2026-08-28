026360e4aa3f485afe8850242ee20eab4d11be72
xwiz <c.opata@robosyslive.com>
2026-08-28 09:32:19 +0100

    fix: roll back communication event ledger

--- migrations/000055_form_communications.down.sql
+++ migrations/000055_form_communications.down.sql
@@ -1,6 +1,7 @@
 BEGIN;
 
 DROP TABLE IF EXISTS form_delivery_attempts;
+DROP TABLE IF EXISTS form_communication_events;
 DROP TABLE IF EXISTS form_communication_templates;
 DROP TABLE IF EXISTS form_communication_profiles;
 DROP TABLE IF EXISTS form_brand_assets;
