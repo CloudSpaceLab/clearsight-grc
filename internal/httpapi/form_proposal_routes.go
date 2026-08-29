aefca6189b18b3fba2c91a758464804ce862c42c
xwiz <c.opata@robosyslive.com>
2026-08-29 18:15:45 +0100

    feat(api): expose governed AI form proposal routes

--- internal/httpapi/form_proposal_routes.go
+++ internal/httpapi/form_proposal_routes.go
@@ -1,12 +1,12 @@
 package httpapi
 
-import (
-	"net/http"
-)
+import "net/http"
 
 func (a *API) formProposalRoutes() []routeSpec {
 	return []routeSpec{
 		write(http.MethodPost, "/api/v1/document-imports/{id}/form-template-proposals", a.createDocumentFormProposal, nil),
+		write(http.MethodPost, "/api/v1/forms/proposals/ai", a.createAIFormProposal, nil),
+		write(http.MethodPost, "/api/v1/forms/templates/{id}/revisions/{version}/ai-proposals", a.createAIFormRevisionProposal, nil),
 		read("/api/v1/forms/proposals/{id}", a.getFormProposal),
 		write(http.MethodPost, "/api/v1/forms/proposals/{id}/accept", a.acceptFormProposal, nil),
 		write(http.MethodPost, "/api/v1/forms/proposals/{id}/reject", a.rejectFormProposal, nil),
