b1ec101ab5dbbbc30173b1cc2831beb8d967125f
Chibueze Opata <copata@venmail.io>
2026-09-02 17:52:39 +0100

    fix wrap compatibility removal migration transaction

--- migrations/000072_remove_vendor_work_address_compatibility.down.sql
+++ migrations/000072_remove_vendor_work_address_compatibility.down.sql
@@ -1,4 +1,8 @@
+BEGIN;
+
 ALTER TABLE third_party_work_requests
     DROP CONSTRAINT third_party_work_requests_request_kind_check,
     ADD CONSTRAINT third_party_work_requests_request_kind_check
         CHECK (request_kind IN ('GENERAL','ADDRESS_VERIFICATION','CERTIFICATION_REFRESH'));
+
+COMMIT;
