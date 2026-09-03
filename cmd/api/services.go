0bbeb7b0a92a6192a439f27a7b51dcad1de24504
xwiz <c.opata@robosyslive.com>
2026-09-03 10:45:56 +0100

    feat(activity): expose activity service in API composition

--- cmd/api/services.go
+++ cmd/api/services.go
@@ -5,6 +5,7 @@ import (
 	"log/slog"
 
 	"github.com/CloudSpaceLab/clearsight-grc/internal/access"
+	"github.com/CloudSpaceLab/clearsight-grc/internal/activity"
 	"github.com/CloudSpaceLab/clearsight-grc/internal/aigovernance"
 	"github.com/CloudSpaceLab/clearsight-grc/internal/authority"
 	"github.com/CloudSpaceLab/clearsight-grc/internal/autonomy"
@@ -65,6 +66,7 @@ type serviceSet struct {
 	AIGovernance                   *aigovernance.Service
 	BankVerticals                  *bankverticals.Service
 	BackgroundJobs                 *operations.Service
+	Activity                       *activity.Service
 	Access                         access.Resolver
 	RuntimeContext                 runtimecontext.Resolver
 	AccessAdmin                    access.Administrator
